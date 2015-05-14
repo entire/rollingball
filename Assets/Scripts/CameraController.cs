b4d2866e8b6de8b6fadcbead5c5e1b962cd012a6
Kosuke Hata <hata.kosuke@gmail.com>
2015-05-14 16:08:13 -0700

    camera shit done

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,14 +2,18 @@
 using System.Collections;
 
 public class CameraController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
+	public GameObject player;
+	
+	private Vector3 offset;
+	
+	void Start ()
+	{
+		offset = transform.position - player.transform.position;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void LateUpdate ()
+	{
+		transform.position = player.transform.position + offset;
 	}
 }
